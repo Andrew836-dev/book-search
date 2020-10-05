9a45b961a85404648cd2a3760d0938001ecac7ce
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-05 14:13:28 +1030

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import SavedList from "./pages/SavedList"
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <header>
+            <h1>Google Books Search Tool</h1>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/saved">Saved</NavLink>
+          </header>
+          <Switch>
+            <Route exact path="/saved" component={SavedList} />
+            <Route component={Homepage} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
