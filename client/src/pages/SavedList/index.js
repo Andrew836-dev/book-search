f9744d021741b7cd869c8f3737aacf57f83fcbb6
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-11 14:41:38 +1030

    updated saved page to show saved results

--- client/src/pages/SavedList/index.js
+++ client/src/pages/SavedList/index.js
@@ -1,6 +1,17 @@
 import React from "react";
+import BookCard from "../../components/BookCard";
 
-function SavedList() {
-  return <p>Saved Book List</p>
+function SavedList(props) {
+  return <>
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          {props.saved.length
+            ? props.saved.map(bookData => <BookCard key={bookData.id} data={bookData} toRemove={props.toRemove} saved={true} />)
+            : <p>No saved books</p>}
+        </div>
+      </div>
+    </div>
+  </>
 }
 export default SavedList;
